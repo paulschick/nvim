f7ec49b668317cf4e33747af69900ed312a36d01
paulschick <paulschick19@gmail.com>
2022-05-01 09:30:03 -0400

    Clear configuration

--- lua/user/utils.lua
+++
@@ -1,8 +0,0 @@
-local utils = {}
-
-utils.bufmap = function(bufnr, mode, key, result)
-	local opts = { noremap = true, silent = true }
-	vim.api.nvim_buf_set_keymap(bufnr, mode, key, "<cmd> " .. result .. "<CR>", opts)
-end
-
-return utils
