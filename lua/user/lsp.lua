f22507cac3d4e89910cb0a2fe57134534ec6f8bb
paulschick <paulschick19@gmail.com>
2022-05-06 08:01:06 -0400

    updates lsp configuration

--- lua/user/lsp.lua
+++ lua/user/lsp.lua
@@ -24,53 +24,32 @@ cmp.setup({
   })
 })
 
--- setup lspconfig
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
-require('lspconfig')['sumneko_lua'].setup {
-  capabilities = capabilities
-}
-require('lspconfig')['bashls'].setup {
-  capabilities = capabilities
-}
-require('lspconfig')['clangd'].setup {
-  capabilities = capabilities
-}
-require('lspconfig')['golangci_lint_ls'].setup {
-  capabilities = capabilities
-}
-require('lspconfig')['gopls'].setup {
-  capabilities = capabilities
-}
-require('lspconfig')['cssls'].setup {
-  capabilities = capabilities
-}
-require('lspconfig')['dockerls'].setup {
-  capabilities = capabilities
-}
-require('lspconfig')['jedi_language_server'].setup {
-  capabilities = capabilities
-}
-require('lspconfig')['jsonls'].setup {
-  capabilities = capabilities
-}
-require('lspconfig')['remark_ls'].setup {
-  capabilities = capabilities
-}
-require('lspconfig')['rust_analyzer'].setup {
-  capabilities = capabilities
-}
-require('lspconfig')['solang'].setup {
-  capabilities = capabilities
-}
-require('lspconfig')['solc'].setup {
-  capabilities = capabilities
-}
-require('lspconfig')['solidity_ls'].setup {
-  capabilities = capabilities
-}
-require('lspconfig')['taplo'].setup {
-  capabilities = capabilities
-}
-require('lspconfig')['tsserver'].setup {
-  capabilities = capabilities
-}
+local opts = { noremap = true, silent = true }
+-- Shorten function name
+local keymap = vim.api.nvim_set_keymap
+
+local function config(_config)
+	return vim.tbl_deep_extend("force", {
+		capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities()),
+		on_attach = function()
+			keymap("n", "gd", ":lua vim.lsp.buf.definition()<CR>", opts)
+			keymap("n", "K", ":lua vim.lsp.buf.hover()<CR>", opts)
+			keymap("n","<leader>vws", ":lua vim.lsp.buf.workspace_symbol()<CR>", opts)
+			keymap("n", "<leader>vd", ":lua vim.diagnostic.open_float()<CR>", opts)
+			keymap("n", "[d", ":lua vim.lsp.diagnostic.goto_next()<CR>", opts)
+			keymap("n", "]d", ":lua vim.lsp.diagnostic.goto_prev()<CR>", opts)
+			keymap("n", "<leader>vca", ":lua vim.lsp.buf.code_action()<CR>", opts)
+			keymap("n", "<leader>vrr", ":lua vim.lsp.buf.references()<CR>", opts)
+			keymap("n", "<leader>vrn", ":lua vim.lsp.buf.rename()<CR>", opts)
+			keymap("n", "<C-h>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
+		end,
+	}, _config or {})
+end
+
+require('lspconfig').tsserver.setup(config())
+require('lspconfig').sumneko_lua.setup(config())
+require('lspconfig').gopls.setup(config())
+require('lspconfig').jedi_language_server.setup(config())
+require('lspconfig').jsonls.setup(config())
+require('lspconfig').remark_ls.setup(config())
+require('lspconfig').solidity_ls.setup(config())
