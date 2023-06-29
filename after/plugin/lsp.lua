563cf68694a5f5991677c22eb2e5de0c968d2c7b
paulschick <pschick@ceiamerica.com>
2023-06-29 09:20:35 -0400

    ocaml/dev - initial ocaml lsp setup

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -3,33 +3,45 @@ local lsp = require('lsp-zero').preset({})
 -- (Optional) Configure lua language server for neovim
 require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
 require('lspconfig').bashls.setup({
-    filetypes = {"sh", "zsh", "bash"},
+    filetypes = { "sh", "zsh", "bash" },
+})
+
+require('lspconfig').ocamllsp.setup({
+    cmd = { "ocamllsp" },
+    filetypes = {
+        "ocaml",
+        "ocaml.menhir",
+        "ocaml.interface",
+        "ocaml.ocamllex",
+        "reason",
+        "dune"
+    },
 })
 
 --'sumneko_lua',
 
 lsp.ensure_installed({
-	'tsserver',
-	'rust_analyzer',
+    'tsserver',
+    'rust_analyzer',
     'bashls'
 })
 
 local cmp = require('cmp')
-local cmp_select = {behavior = cmp.SelectBehavior.Select}
+local cmp_select = { behavior = cmp.SelectBehavior.Select }
 local cmp_mappings = lsp.defaults.cmp_mappings({
-	['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-	['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-	['<C-y>'] = cmp.mapping.confirm({ select = true }),
-	['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+    ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+    ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+    ['<C-Space>'] = cmp.mapping.complete(),
 })
 
 -- Turn off the icons
 lsp.set_preferences({
-	sign_icons = { }
+    sign_icons = {}
 })
 
 lsp.setup_nvim_cmp({
-	mapping = cmp_mappings
+    mapping = cmp_mappings
 })
 
 --[[
@@ -41,20 +53,21 @@ lsp.setup_nvim_cmp({
 	For example, if you use gd here, then it's going to prefer to use the
 	lsp go to definition. If it doesn't have lsp, then vim will try to
 	go to definition.
-]]--
+]]
+--
 lsp.on_attach(function(client, bufnr)
-	local opts = {buffer = bufnr, remap = false}
-	--lsp.default_keymaps({buffer = bufnr})
-	vim.keymap.set('n', 'gd', function() vim.lsp.buf.definition() end, opts)
-	vim.keymap.set('n', 'K', function() vim.lsp.buf.hover() end, opts)
-	vim.keymap.set('n', '<leader>vws', function() vim.lsp.buf.workspace_symbol() end, opts)
-	vim.keymap.set('n', '<leader>vd', function() vim.diagnostic.open_float() end, opts)
-	vim.keymap.set('n', '[d', function() vim.diagnostic.goto_next() end, opts)
-	vim.keymap.set('n', ']d', function() vim.diagnostic.goto_prev() end, opts)
-	vim.keymap.set('n', '<leader>vca', function() vim.lsp.buf.code_action() end, opts)
-	vim.keymap.set('n', '<leader>vrr', function() vim.lsp.buf.references() end, opts)
-	vim.keymap.set('n', '<leader>vrn', function() vim.lsp.buf.rename() end, opts)
-	vim.keymap.set('n', '<C-h>', function() vim.lsp.buf.signature_help() end, opts)
+    local opts = { buffer = bufnr, remap = false }
+    --lsp.default_keymaps({buffer = bufnr})
+    vim.keymap.set('n', 'gd', function() vim.lsp.buf.definition() end, opts)
+    vim.keymap.set('n', 'K', function() vim.lsp.buf.hover() end, opts)
+    vim.keymap.set('n', '<leader>vws', function() vim.lsp.buf.workspace_symbol() end, opts)
+    vim.keymap.set('n', '<leader>vd', function() vim.diagnostic.open_float() end, opts)
+    vim.keymap.set('n', '[d', function() vim.diagnostic.goto_next() end, opts)
+    vim.keymap.set('n', ']d', function() vim.diagnostic.goto_prev() end, opts)
+    vim.keymap.set('n', '<leader>vca', function() vim.lsp.buf.code_action() end, opts)
+    vim.keymap.set('n', '<leader>vrr', function() vim.lsp.buf.references() end, opts)
+    vim.keymap.set('n', '<leader>vrn', function() vim.lsp.buf.rename() end, opts)
+    vim.keymap.set('n', '<C-h>', function() vim.lsp.buf.signature_help() end, opts)
 end)
 
 lsp.setup()
